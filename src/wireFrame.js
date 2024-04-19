b6ff9d08189298d040938f785910183229ddf28e
Faith Mbah <faithmbah10@gmail.com>
2024-04-19 22:02:30 +0100

    updated prototype

--- src/wireFrame.js
+++ src/wireFrame.js
@@ -2,8 +2,16 @@ import React from 'react';
 import bci from './images/bc.png'; 
 import solution from './images/solution.png'; 
 import Rect from './images/Rect.png'
-import Header from './component/header';
+import present from './images/present.png';
+import Rectan from './images/Rectan.png';
+import plan from './images/plan.png';
 import proto from './images/proto.png';
+import imgA from './images/imgA.jpeg'
+import imgB from './images/imgB.jpeg'
+import imgC from './images/imgC.jpeg'
+import imgD from './images/imgD.jpeg'
+import imgE from './images/imgE.jpeg'
+import Header from './component/header';
 import Menu from './component/menu';
 import cloud from './images/cloud.png'
 import { useNavigate } from 'react-router-dom';
@@ -34,26 +42,26 @@ function WireFrame ()  {
                <div type="button" className='clod'><img src={cloud} className='imgC'></img>Upload</div> 
 
                 <div className='imgbox'>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
+                  <img src={proto} className='imgA' ></img>
+                  <img src={plan} className='imgA' ></img>
+                  <img src={imgA} className='imgA' ></img>
+                  <img src={imgB} className='imgA' ></img>
+                  <img src={imgC} className='imgA' ></img>
             
+                  <img src={Rectan} className='imgA' ></img>
                   <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
-                  <img src={Rect} className='imgA' ></img>
+                  <img src={imgE} className='imgA' ></img>
+                  <img src={imgD} className='imgA' ></img>
+                  <img src={present} className='imgA' ></img>
                   </div>
                 </div> 
                 <div class="container-boxj">
-                  <img src={proto} className='imgh'></img>    
-                  <img src={proto} className='imgh'></img>   
+                  <img src={present} className='imgh'></img>    
+                  <img src={plan} className='imgh'></img>   
                   <img src={proto} className='imgh'></img> 
-                  <img src={proto} className='imgh'></img> 
-                  <img src={proto} className='imgh'></img> 
-                  <img src={proto} className='imgh'></img>              
+                  <img src={Rectan} className='imgh'></img> 
+                  <img src={Rect} className='imgh'></img> 
+                  <img src={imgC} className='imgh'></img>              
                 </div>         
             </div>
            
