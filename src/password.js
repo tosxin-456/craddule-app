da5e2c5bfdf5270c03ab01055cb898575196a4cb
Faith Mbah <81395403+Faithmbah@users.noreply.github.com>
2024-04-24 03:13:15 +0100

    Password added

---
+++ src/password.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { useNavigate } from 'react-router-dom';
+import { text } from '@fortawesome/fontawesome-svg-core';
+
+function Password() {
+
+    //useEffect(() => {
+    //     const wow = new WOW.WOW();
+    //     wow.init();
+    //   }, []);
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    
+  
+    const handleTogglePassword = () => {
+      setShowPassword(!showPassword);
+    };
+    const navigate = useNavigate()
+    const onClickHandler = () => navigate(`/signUp`)
+    const onClickHandler1 = () => navigate(``)
+  return (
+
+<div className='container'>
+   <div className='wholeP'>
+    <div className='row'>
+        <div className='col-md-6'>
+          <div className='loginHp'>
+            <p className='lgT'>Password</p>
+            <p className='lgT2'>Input Auto-Generated password sent to your email</p>
+
+            <div className="inputs-container">
+                <label htmlFor="password" className='lab'>Password</label>
+            
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="custom-input"
+                  />
+                   <span className="password-toggle" onClick={handleTogglePassword}>
+                    {showPassword ? 'Hide' : 'Show'}
+                  </span>
+                  
+              </div>
+
+              <button className='btn loginBtn' onClick={onClickHandler1}>Proceed</button>
+              <p className='lab'>Already on Craddule? <a href=''onClick={onClickHandler}>Sign Up</a> now!</p>
+          </div>
+          
+        </div>
+
+        <div className='col-md-6'>
+          <div className='halfWh'>
+            <div className='blurry'>
+              <p>You will never do anything in this world without courage. It is the greatest quality of the mind next to honor</p>
+
+              
+            </div>
+          </div>
+        </div>
+    </div>
+    
+      
+   </div>
+
+</div>
+  );
+}
+
+export default Password;
