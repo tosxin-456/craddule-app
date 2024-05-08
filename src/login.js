af7912c1a92215f6dde6507c4118d4c8c2c1e1f5
a-mobi-hq <161224511+a-mobi-hq@users.noreply.github.com>
2024-05-08 19:12:16 +0100

    Update login.js

--- src/login.js
+++ src/login.js
@@ -86,15 +86,16 @@ function Login() {
               console.log(responseData.user);
             const userStatus = responseData.user.status;
             console.log(responseData.user.status);
+               navigate(`/introduction1`);
               
-            if(userStatus === 'deactivated'){
-              toast.error("This Account as been Deactivated");
-            }else{
-              localStorage.setItem('access_token', token);
-              console.log('Access Token:', token);
-              localStorage.setItem('access_token', token);
-              navigate(`/introduction1`);
-            }
+            // if(userStatus === 'deactivated'){
+            //   toast.error("This Account as been Deactivated");
+            // }else{
+            //   localStorage.setItem('access_token', token);
+            //   console.log('Access Token:', token);
+            //   localStorage.setItem('access_token', token);
+            //   navigate(`/introduction1`);
+            // }
            
              // navigate(`/introduction1`)
           } else {
