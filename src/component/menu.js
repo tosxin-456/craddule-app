9ac007f5d6abe950fd941d2874d2a6ba55630e15
Faith Mbah <faithmbah10@gmail.com>
2024-05-23 17:31:22 +0100

    update

--- src/component/menu.js
+++ src/component/menu.js
@@ -31,6 +31,7 @@ function Menu () {
   const onClickHandler23 = () => navigate(`/profile`);
   const onClickHandler24 = () => navigate(`/multipleGraph`);
   const onClickHandler25 = () => navigate(`/comparative`);
+  const onClickHandler26 = () => navigate(`/timelineBuilder`);
 
 
   const [isClosed, setIsClosed] = useState(false);
@@ -99,7 +100,7 @@ function Menu () {
               <li className='innr' onClick={onClickHandler24}>Custom Financial Projection</li>
               <li className='innr'onClick={onClickHandler18}>Discounted Cash Flow (DCF)</li>
               <li className='innr'onClick={onClickHandler25}>Comparative Analysis</li>
-              <li className='innr'>Project Timeline Builder</li>
+              <li className='innr'onClick={onClickHandler26}>Project Timeline Builder</li>
               <li className='innr' onClick={onClickHandler20}>Go/ No-Go gate</li>
               <li className='innr'onClick={onClickHandler11}>Summary (PDF)</li>
               {/* Add more submenu items as needed */}
