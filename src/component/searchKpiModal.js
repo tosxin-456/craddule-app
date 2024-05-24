1ef41eb74444a23e82b84f87bbc98a69273be0fe
Faith Mbah <faithmbah10@gmail.com>
2024-05-24 19:12:44 +0100

    update

--- src/component/searchKpiModal.js
+++ src/component/searchKpiModal.js
@@ -11,13 +11,16 @@ export default function SearchKpiModal ({open, onClose})  {
     return ReactDOM.createPortal(
         <>
         <div className='modalOv' >
-           <div className='modalSt1'>
+           <div className='modalSt'>
                 <p className='closeIcon' onClick={onClose} type='button'>X</p>
               <p className='txt2'>Add KPI</p>
               <div className='whiteKpi1'>
-              <input className='inptT' type="text" placeholder="Type to search.."></input>
+              <input className='inptT1' type="text" placeholder="Type to search.."></input>
               
+            
              <div className='searchKpiBox'>
+
+               <div className="">
              <p className='selectKpi'>Or select from the list below</p>
             <div className='divButton'>
             <div><button className="btn-primary dropdown-toggle divBut" type="button" data-toggle="dropdown">Fintech</button></div>
@@ -30,12 +33,13 @@ export default function SearchKpiModal ({open, onClose})  {
             <div><button className="btn-primary dropdown-toggle divBut" type="button" data-toggle="dropdown">Agriculture</button></div>
             <div><button className="btn-primary dropdown-toggle divBut" type="button" data-toggle="dropdown">Finance</button></div>
             </div>
-            <div className='flex-container sideDiv'>
-               <p className='sideDivN'>Selected KPIs</p>
-               <div className='sideDivD'></div>
             </div>
-               
-    </div>      
+            <div className='sideDiv'>
+               <p className='sideDivN'>Selected KPIs</p>
+               <input className='sideDivD'></input>
+            </div>
+            </div>  
+    
              <button className="btn btn-primary kpib">Cancel</button>
               <button className="btn btn-primary kpibA">Add to my account</button>
               </div>          
