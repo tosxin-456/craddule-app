3e51c9c637319acff3d36064da7bd4c0b5b24990
a-mobi-hq <161224511+a-mobi-hq@users.noreply.github.com>
2024-05-25 08:33:21 +0100

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // apiConfig.js
-//const API_BASE_URL = 'https://craddd-cx3n.onrender.com';
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://craddd-cx3n.onrender.com';
+//const API_BASE_URL = 'http://localhost:3001';
 
 export default API_BASE_URL;
