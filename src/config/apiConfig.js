7ae6dbe258dbd637150482dc0922c53b27063c56
a-mobi-hq <161224511+a-mobi-hq@users.noreply.github.com>
2024-11-07 04:37:13 +0100

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -8,7 +8,7 @@ const APP_BASE_URL = 'https://demo.craddule.com';
 const APP_REFER_URL = 'https://craddule.com';
 // const API_BASE_URL = 'https://demo.cradduleapi.com.ng';
 // const API_BASE_URL = 'https://api.cradduleapi.com.ng';
-const API_BASE_URL = 'http://localhost:3001'
+//const API_BASE_URL = 'http://localhost:3001'
 module.exports= {API_BASE_URL, 
     APP_BASE_URL, APP_REFER_URL, API_IMAGE_BASE_URL
 } 
