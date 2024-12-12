747dfce09d5124ffd1b5cc69ad428a19ba3f3381
Tosin Ekundayo <tosinekshally@gmail.com>
2024-12-12 19:39:48 +0100

    added the ui

--- src/component/modalStartStop.js
+++ src/component/modalStartStop.js
@@ -1,45 +1,45 @@
-import React, { useState }from  "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { jwtDecode } from "jwt-decode";
 import { HiOutlineArrowSmallRight } from "react-icons/hi2";
 import { useNavigate } from 'react-router-dom';
-import {API_BASE_URL} from '../config/apiConfig';
+import { API_BASE_URL } from '../config/apiConfig';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
-export default function ModalStart({ open, onClose}){
+export default function ModalStart({ open, onClose }) {
 
   const navigate = useNavigate()
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');  
+  const [errorMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState({
     projectName: '',
   });
   const [formQData, setFormQData] = useState({
     answer: '',
   });
-  const category ="NONE";
-  const subCategoryPassed ="NONE";
+  const category = "NONE";
+  const subCategoryPassed = "NONE";
   const [question, setQuestion] = useState('');
   const [showProjectModal, setShowProjectModal] = useState(true)
   const [showQuestionsModal, setShowQuestionsModal] = useState(false)
-
+  const [error, setError] = useState(" ")
   const access_token = localStorage.getItem('access_token');
   const decodedToken = jwtDecode(access_token);
   const userId = decodedToken.userId;
 
   // console.log(userId);
 
-  
+
   const fetchUnansweredQuestion = async (projectId) => {
     setErrorMessage('');
     try {
-      const response = await fetch(API_BASE_URL+`/api/new/question/${userId}/${projectId}/${category}/${subCategoryPassed}`);
+      const response = await fetch(API_BASE_URL + `/api/new/question/${userId}/${projectId}/${category}/${subCategoryPassed}`);
       if (response.status === 200) {
         const data = await response.json();
         console.log(data);
         if (!data.data) {
-            navigate(`/accelerate`);
+          navigate(`/accelerate`);
         } else {
           console.log(data.data.questionOrder);
           setQuestion(data.data);
@@ -56,7 +56,7 @@ export default function ModalStart({ open, onClose}){
       }
     } catch (error) {
       console.error('An error occurred:', error.message);
-    } finally{
+    } finally {
       setLoading(false)
     }
   };
@@ -89,15 +89,15 @@ export default function ModalStart({ open, onClose}){
     onClose();
     console.log('closing');
   }
-    
+
   const createProject = async (data) => {
     setLoading(true);
     try {
-    // Include user ID in the data object
-    data.userId = userId;  
-    console.log(data);
-    console.log(JSON.stringify(data));
-      const response = await fetch(API_BASE_URL+'/api/project', {
+      // Include user ID in the data object
+      data.userId = userId;
+      console.log(data);
+      console.log(JSON.stringify(data));
+      const response = await fetch(API_BASE_URL + '/api/project', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -112,11 +112,11 @@ export default function ModalStart({ open, onClose}){
         console.log(response.status);
         console.log(response);
 
-        
+
         const responseData = await response.json();
         // setLoading(false);
         const projectId = responseData.data._id;
-        localStorage.setItem("nProject",projectId);
+        localStorage.setItem("nProject", projectId);
         // navigate(`/firstQuestion`);
         console.log(responseData); // Parse JSON response
         console.log('Project created successfully');
@@ -127,40 +127,40 @@ export default function ModalStart({ open, onClose}){
         setLoading(false);
         setErrorMessage(result['error']);
         setTimeout(() => {
-            setErrorMessage('');
-          }, 5000);
-        
-          console.error('Error:', result['error']);
+          setErrorMessage('');
+        }, 5000);
+        setError(result['error'])
+        console.error('Error:', result['error']);
       }
     } catch (error) {
-        setLoading(false);
-        setErrorMessage(error);
-        setTimeout(() => {
-            setErrorMessage('');
-          }, 5000);
+      setLoading(false);
+      setErrorMessage(error);
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 5000);
       console.error('An error occurred:', error);
     }
   };
 
   const createAnswer = async (data) => {
     setLoading(true);
-    const projectId = localStorage.getItem('nProject'); 
+    const projectId = localStorage.getItem('nProject');
     try {
-      data.userId = userId;  
-      data.questionId = question._id; 
-      data.projectId = projectId; 
-      data.questionType = category; 
-      data.questionSubType = subCategoryPassed; 
+      data.userId = userId;
+      data.questionId = question._id;
+      data.projectId = projectId;
+      data.questionType = category;
+      data.questionSubType = subCategoryPassed;
 
       console.log(data);
-      
-      const response = await fetch(API_BASE_URL+'/api/answer', {
+
+      const response = await fetch(API_BASE_URL + '/api/answer', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${access_token}`,
         },
-        body: JSON.stringify({data}),
+        body: JSON.stringify({ data }),
       });
 
       if (response.status === 200) {
@@ -182,61 +182,98 @@ export default function ModalStart({ open, onClose}){
     }
   };
 
-  if(!open) return null
+  if (!open) return null
   return ReactDOM.createPortal(
     <>
       <div className="modalOv"></div>
-      {showProjectModal && 
+      {showProjectModal &&
         <div className="modalSt ">
-        <div className="relative flex justify-end mb-3">
-          <span onClick={handleProceed} className="block w-fit p-2 px-3 rounded-md border border-blue50 text-gray900 cursor-pointer">X</span>
-        </div>
-        <h4 className="text-center text-black400 mb-[30px]">New project name</h4>
-        { errorMessage &&  <p className="createER">Project name is empty</p>}
-          
-        <form onSubmit={handleSubmit} className="px-[40px]">
-        <div className="">
-          {/* <label htmlFor="projectName" className="creT">Create Project</label> */}
-          <input
-              type="text"
-              id="projectName"
-              className="w-full p18 py-[20px] ps-[40px] rounded-[15px] bg-blue-50"
-              placeholder="Project Name"
-              value={formData.projectName}
-              onChange={handleChange}
+          <div className="relative flex justify-end mb-3">
+            <span onClick={handleProceed} className="block w-fit p-2 px-3 rounded-md border border-blue50 text-gray900 cursor-pointer">X</span>
+          </div>
+          <h4 className="text-center text-black400 mb-[30px]">New project name</h4>
+          {errorMessage && <p className="createER">{error}</p>}
 
-          />
-          </div>
-          <button type="submit" className="w-full bg-blue600 text-white rounded-[30px] mt-[40px] py-[14px]" disabled={loading}>
-          { loading && <FontAwesomeIcon icon={faCircleNotch} className='fa-spin'/>}
-          { !loading &&  <h4 className=''>Continue</h4>}
-              
-          </button>
-          
+          <form onSubmit={handleSubmit} className="px-[40px]">
+            <div className="">
+              {/* <label htmlFor="projectName" className="creT">Create Project</label> */}
+              <input
+                type="text"
+                id="projectName"
+                className="w-full p18 py-[20px] ps-[40px] rounded-[15px] bg-blue-50"
+                placeholder="Project Name"
+                value={formData.projectName}
+                onChange={handleChange}
+
+              />
+            </div>
+            <div className="mt-[20px]">
+              <select
+                id="industry"
+                className="w-full p18 py-[20px] ps-[40px] rounded-[15px] bg-blue-50"
+                value={formData.industry}
+                onChange={handleChange}
+              >
+                <option value="">Select an Industry</option>
+                <option value="Technology">Technology</option>
+                <option value="Healthcare">Healthcare</option>
+                <option value="Finance">Finance</option>
+                <option value="Education">Education</option>
+                <option value="Retail">Retail</option>
+                <option value="Manufacturing">Manufacturing</option>
+                <option value="Real Estate">Real Estate</option>
+                <option value="Media">Media</option>
+                <option value="Hospitality">Hospitality</option>
+                <option value="Transportation">Transportation</option>
+                <option value="Energy">Energy</option>
+                <option value="Agriculture">Agriculture</option>
+                <option value="Telecommunications">Telecommunications</option>
+                <option value="Construction">Construction</option>
+                <option value="Legal Services">Legal Services</option>
+                <option value="Government">Government</option>
+                <option value="Nonprofit">Nonprofit</option>
+                <option value="Aerospace">Aerospace</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Logistics">Logistics</option>
+                <option value="Automotive">Automotive</option>
+                <option value="Food and Beverage">Food and Beverage</option>
+                <option value="Fashion">Fashion</option>
+                <option value="Pharmaceuticals">Pharmaceuticals</option>
+                <option value="Environmental Services">Environmental Services</option>
+                <option value="Insurance">Insurance</option>
+                <option value="Sports and Recreation">Sports and Recreation</option>
+              </select>
+            </div>
+            <button type="submit" className="w-full bg-blue600 text-white rounded-[30px] mt-[40px] py-[14px]" disabled={loading}>
+              {loading && <FontAwesomeIcon icon={faCircleNotch} className='fa-spin' />}
+              {!loading && <h4 className=''>Continue</h4>}
+
+            </button>
+
           </form>
-          
-      </div>}
-      {showQuestionsModal && 
-      <div className="modalSt">
-        {/* <h4 className="text-center text-black400 mb-[30px]">New project name</h4> */}
-        { errorMessage &&  <p className="createER">Answer is empty</p>}
-            
-        <form onSubmit={handleQSubmit}>
-          <div className="">
-            <input
-              type="text"
-              id="answer"
-              className="w-full p18 py-[20px] ps-[40px] rounded-[15px] bg-blue-50"
-              placeholder={question.question}
-              value={formQData.answer} onChange={handleQChange}
-            />
-          </div>
-          <button type="submit" className="w-full bg-blue600 text-white rounded-[30px] mt-[40px] py-[14px]" disabled={loading}>
-            { loading && <FontAwesomeIcon icon={faCircleNotch} className='fa-spin'/>}
-            { !loading &&  <h4 className=''>Continue</h4>}
-          </button>
-        </form>
-      </div>}
+
+        </div>}
+      {showQuestionsModal &&
+        <div className="modalSt">
+          {/* <h4 className="text-center text-black400 mb-[30px]">New project name</h4> */}
+          {errorMessage && <p className="createER">Answer is empty</p>}
+
+          <form onSubmit={handleQSubmit}>
+            <div className="">
+              <input
+                type="text"
+                id="answer"
+                className="w-full p18 py-[20px] ps-[40px] rounded-[15px] bg-blue-50"
+                placeholder={question.question}
+                value={formQData.answer} onChange={handleQChange}
+              />
+            </div>
+            <button type="submit" className="w-full bg-blue600 text-white rounded-[30px] mt-[40px] py-[14px]" disabled={loading}>
+              {loading && <FontAwesomeIcon icon={faCircleNotch} className='fa-spin' />}
+              {!loading && <h4 className=''>Continue</h4>}
+            </button>
+          </form>
+        </div>}
     </>,
     document.getElementById('portal')
   )
